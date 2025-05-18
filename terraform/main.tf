68c050cbcd90e187a2b81bd5634152c77bb1a00a
noeleon21 <noelgens@gmail.com>
2025-05-18 16:02:20 +1000

    updated main tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -52,19 +52,24 @@ resource "aws_s3_bucket_website_configuration" "example" {
 resource "aws_s3_bucket_policy" "public_read_policy" {
   bucket = aws_s3_bucket.bucket_name.id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = [
-          "s3:GetObject",
-          "s3:ListBucket"
-        ],
-        Resource = "${aws_s3_bucket.bucket_name.arn}/*"
-      }
-    ]
-  })
+policy = jsonencode({
+  Version = "2012-10-17",
+  Statement = [
+    {
+      Sid       = "PublicReadGetObject",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.bucket_name.arn}/*"
+    },
+    {
+      Sid       = "PublicListBucket",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:ListBucket",
+      Resource  = aws_s3_bucket.bucket_name.arn
+    }
+  ]
+})
+
 }
