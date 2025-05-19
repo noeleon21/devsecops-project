2498d1d723a4cb300672377670eae9768644bf71
noeleon21 <noelgens@gmail.com>
2025-05-19 19:18:35 +1000

    addded public address on ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,10 +17,14 @@ resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id = aws_subnet.public_subnet.id
-
+  associate_public_ip_address = true
   tags = {
     Name = var.instance_name
   }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my-vpc.id
 }
 
 resource "aws_vpc" "my-vpc" {
@@ -30,7 +34,24 @@ resource "aws_vpc" "my-vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my-vpc.id
   cidr_block        = aws_vpc.my-vpc.cidr_block
+  map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+  
 }
 
 resource "aws_security_group" "allowedports" {
