6e6df2b2de04b2d27aae568982d5d260591366a7
noeleon21 <noelgens@gmail.com>
2025-05-25 21:00:49 +1000

    commnted out rds endpoint output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,7 +13,7 @@ output "ec2_public_ip" {
   value       = aws_instance.web.public_ip
 }
 
-output "rds_endpoint" {
-  description = "RDS MySQL endpoint"
-  value       = aws_db_instance.mysql.endpoint
-}
+# output "rds_endpoint" {
+#   description = "RDS MySQL endpoint"
+#   value       = aws_db_instance.mysql.endpoint
+# }
