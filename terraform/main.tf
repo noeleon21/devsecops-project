e8f08e1e409d79723c42c9df84a46dd96f047880
noeleon21 <noelgens@gmail.com>
2025-05-28 20:00:21 +1000

    enabled dns

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,6 +51,8 @@ resource "aws_internet_gateway" "gw" {
 
 resource "aws_vpc" "my-vpc" {
   cidr_block = var.vpc_cidr
+  enable_dns_support   = true     
+  enable_dns_hostnames = true     
 }
 
 # resource "aws_subnet" "public_subnet" {
