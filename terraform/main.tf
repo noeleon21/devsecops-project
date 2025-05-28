6895d0495ec2c93578a8102650dd18a9f0612ba7
noeleon21 <noelgens@gmail.com>
2025-05-28 16:38:20 +1000

    changed subnets

--- terraform/main.tf
+++ terraform/main.tf
@@ -131,9 +131,16 @@ resource "aws_db_instance" "default" {
   vpc_security_group_ids = [aws_security_group.allowedports.id]
   db_subnet_group_name =  aws_db_subnet_group.default.name
 }
-resource "aws_subnet" "public_subnet_2" {
+
+resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+}
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.my-vpc.id
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1b"
 }
