3386ebbfd2f991569d19f55550f180216c3f04d3
noeleon21 <noelgens@gmail.com>
2025-05-28 19:05:15 +1000

    tested another subent

--- terraform/main.tf
+++ terraform/main.tf
@@ -134,13 +134,13 @@ resource "aws_db_instance" "default" {
 
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.0.1/16"
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1a"
 }
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.0.2/16"
+  cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "us-east-1b"
 }
