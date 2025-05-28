47c0821d8cb5aad43cb4432e5d8821fe765eba8c
noeleon21 <noelgens@gmail.com>
2025-05-28 19:36:39 +1000

    test another subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -134,15 +134,15 @@ resource "aws_db_instance" "default" {
 
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.0.0/20"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-1b"
 }
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.my-vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.16.0/20"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1b"
+  availability_zone       = "us-east-1c"
 }
 resource "aws_db_subnet_group" "default" {
   name       = "default-subnet-group"
